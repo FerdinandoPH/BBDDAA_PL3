abfb16a82a0bb383b3ce77190e3cf1aaae7668c0
FerdinandoPH <perez.holguin@gmail.com>
2025-05-17 17:37:02 +0200

    12

---
+++ PL3_c12.sql
@@ -0,0 +1,19 @@
+-- Ejecutar como usuario1, usuario2 o usuario3
+\c telpark
+
+BEGIN;
+
+INSERT INTO clientes VALUES(1, 'Mengano', 'Menganez', 'menganito@example.ex', 918856931, 'Cantabria');
+
+INSERT INTO vehiculos VALUES(1, '6789HPT', 'Renault', 'Scenic', 'Rojo', 1);
+
+UPDATE vehiculos WHERE vehiculoid = 1 SET matricula = '1234MMG';
+
+INSERT INTO clientes VALUES(2, 'Zutano', 'Zutanez', 'zutanito@example.ex', 987651234, 'Madrid');
+
+INSERT INTO vehiculos VALUES(3, '4567HPG', 'Seat', 'Panda', 'Blanco', 2);
+
+COMMIT;
+
+SELECT * FROM clientes;
+SELECT * FROM vehiculos;
--- PL3_c9.sql
+++ PL3_c9.sql
@@ -1,6 +1,10 @@
+-- Ejecutar como usuario2
 \c telpark
 
 SELECT * FROM clientes WHERE clienteid=0;
 SELECT * FROM vehiculos WHERE vehiculoid=0;
 
+\q
+
+-- ver como SU
 SELECT * FROM pg_stat_activity WHERE state='idle' OR state='idle in transaction';
